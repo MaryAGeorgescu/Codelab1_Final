c243da82131879a67cac04f05a31237f4c112450
maryageorgescu <mg3200@nyu.edu>
2019-05-19 19:35:31 -0400

    Trying to add some background music that persists through the scenes.

---
+++ CodeLab1_final/Assets/Scripts/BackgroundMusicManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}

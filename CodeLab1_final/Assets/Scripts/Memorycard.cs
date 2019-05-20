45ec6d3134a236fba859a1d99e281185a5386cc1
maryageorgescu <mg3200@nyu.edu>
2019-05-20 17:01:37 -0400

    And now for something completely different.

---
+++ CodeLab1_final/Assets/Scripts/Memorycard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Intent: This script hides the card back when the card is clicked
+//Usage: Place on
+
+
+public class Memorycard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack; //our variable in the inspector
+    
+    
+    private void OnMouseDown() 
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+}

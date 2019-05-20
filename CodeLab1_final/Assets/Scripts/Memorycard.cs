ce4f7c52c3b931f722423a0e151ce5b67549f862
maryageorgescu <mg3200@nyu.edu>
2019-05-20 17:24:23 -0400

    Added a new public method to memory card script

--- CodeLab1_final/Assets/Scripts/Memorycard.cs
+++ CodeLab1_final/Assets/Scripts/Memorycard.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 //Intent: This script hides the card back when the card is clicked
 //Usage: Place on
@@ -17,4 +18,20 @@ public class Memorycard : MonoBehaviour
             cardBack.SetActive(false);
         }
     }
+
+    [SerializeField] private SceneManager controller;
+
+    private int _id;
+
+    public int id
+    {
+        get{return _id}
+    }
+
+    public void SetCard (int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
 }
+
--- CodeLab1_final/Assets/Scripts/SceneManager.cs
+++ CodeLab1_final/Assets/Scripts/SceneManager.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class SceneManager : MonoBehaviour
 {
+    [SerializeField] private Memorycard originalCard;
+
+    [SerializeField] private Sprite[] images;
     // Start is called before the first frame update
     void Start()
     {
-        
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
